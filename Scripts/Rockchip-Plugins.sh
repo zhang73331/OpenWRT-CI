b203b6077b1f13885c933fc33e1a27288266c3ee
zhang73331 <107366211+zhang73331@users.noreply.github.com>
2023-11-23 16:31:37 +0800

    Update Rockchip-Plugins.sh

--- Scripts/Rockchip-Plugins.sh
+++ Scripts/Rockchip-Plugins.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+#netspeedtest
+git clone --depth=1 --single-branch https://github.com/sirpdboy/netspeedtest.git
+#Linkease
+git clone --depth=1 --single-branch https://github.com/linkease/istore.git
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages.git
+git clone --depth=1 --single-branch https://github.com/linkease/nas-packages-luci.git
 #Design Theme
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "main" || echo "js") https://github.com/gngpp/luci-theme-design.git
 git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
 #Argon Theme
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
+#kenzok8-openwrt-packages
+git clone --depth=1 --single-branch https://github.com/kenzok8/openwrt-packages.git
+git clone --depth=1 --single-branch https://github.com/kenzok8/small.git
 #Pass Wall
 git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall.git
 git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall2.git
