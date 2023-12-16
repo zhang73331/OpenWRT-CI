1524f3f3c790609799cd9df5f32ab3ee7aeada5e
zhang73331 <107366211+zhang73331@users.noreply.github.com>
2023-12-16 21:18:37 +0800

    Update Rockchip-Plugins.sh

--- Scripts/Rockchip-Plugins.sh
+++ Scripts/Rockchip-Plugins.sh
@@ -12,9 +12,7 @@ git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-con
 #Argon Theme
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth=1 --single-branch --branch $(echo $OWRT_URL | grep -iq "lede" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
-#kenzok8-openwrt-packages
-#git clone --depth=1 --single-branch https://github.com/kenzok8/openwrt-packages.git
-#git clone --depth=1 --single-branch https://github.com/kenzok8/small.git
+
 #Pass Wall
 git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall.git
 git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall2.git
